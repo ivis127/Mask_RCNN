681a6ba36faacb1551646ca93adfb68e2a052acc
ivis127 <53078834+ivis127@users.noreply.github.com>
2023-06-24 18:48:55 +0900

    Update utils.py

--- mrcnn/utils.py
+++ mrcnn/utils.py
@@ -895,12 +895,12 @@ def resize(image, output_shape, order=1, mode='constant', cval=0, clip=True,
     version. And it provides a central place to control resizing defaults.
     """
     if LooseVersion(skimage.__version__) >= LooseVersion("0.17"):
-        if np.issubdtype(image.dtype, np.bool):
-            return skimage.transform.resize(
-                image, output_shape,
-                order=0, mode=mode, cval=cval, clip=clip,
-                preserve_range=preserve_range, anti_aliasing=anti_aliasing,
-                anti_aliasing_sigma=anti_aliasing_sigma)
+        #if np.issubdtype(image.dtype, np.bool):
+         #   return skimage.transform.resize(
+          #      image, output_shape,
+           #     order=0, mode=mode, cval=cval, clip=clip,
+            #    preserve_range=preserve_range, anti_aliasing=anti_aliasing,
+             #   anti_aliasing_sigma=anti_aliasing_sigma)
         return skimage.transform.resize(
             image, output_shape,
             order=order, mode=mode, cval=cval, clip=clip,
