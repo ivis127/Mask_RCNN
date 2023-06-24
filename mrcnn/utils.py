523e7bc0231c30e968e51ac72c2769b677a56284
ivis127 <53078834+ivis127@users.noreply.github.com>
2023-06-24 18:41:06 +0900

    Update utils.py

--- mrcnn/utils.py
+++ mrcnn/utils.py
@@ -896,11 +896,11 @@ def resize(image, output_shape, order=1, mode='constant', cval=0, clip=True,
     """
     if LooseVersion(skimage.__version__) >= LooseVersion("0.17"):
         if np.issubdtype(image.dtype, np.bool):
-            return skimage.util.img_as_bool(skimage.transform.resize(
-                image.astype(np.float32), output_shape,
-                order=order, mode=mode, cval=cval, clip=clip,
+            return skimage.transform.resize(
+                image, output_shape,
+                order=0, mode=mode, cval=cval, clip=clip,
                 preserve_range=preserve_range, anti_aliasing=anti_aliasing,
-                anti_aliasing_sigma=anti_aliasing_sigma))
+                anti_aliasing_sigma=anti_aliasing_sigma)
         return skimage.transform.resize(
             image, output_shape,
             order=order, mode=mode, cval=cval, clip=clip,
